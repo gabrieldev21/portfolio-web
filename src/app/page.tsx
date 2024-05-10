bb207b9c182254d9ac629d10e6c8ec30d10b6a13
gabrieldev21 <gabrielp.amorim@live.com>
2024-05-10 11:26:04 -0300

    feat: verificar cookie no server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link';
 
 import { Header } from '@/components';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  if (!cookies().get('access')) redirect('/login');
+
   return (
     <>
-      <Header cookie={cookies().get('access')} />
+      <Header />
       <main className="flex-col sm:flex-row items-center pt-8 justify-evenly max-h-screen max-w-screen">
         <section className="flex flex-col text-white">
           <Link href="/login">Back to Login</Link>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,7 @@
-'use client';
-
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect } from 'react';
-import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
-import { useRouter } from 'next/navigation';
 
-export const Header = ({ cookie }: { cookie?: RequestCookie | undefined }) => {
-  const router = useRouter();
-  useEffect(() => {
-    if (!cookie) router.push('/login');
-  }, []);
-
+export const Header = () => {
   return (
     <main className="flex justify-between px-[13vw] pt-5 [&>*]:cursor-pointer ">
       <section className="[&>a]:hover:text-lime-500 text-white">
