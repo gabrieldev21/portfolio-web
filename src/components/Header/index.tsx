da9056727b3a2ad91ca7e49b2880f192f9004ae8
gabrieldev21 <gabrielp.amorim@live.com>
2024-06-05 12:23:52 -0300

    fix: ajuste responsividade no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,54 +1,23 @@
 'use client';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-
 import useLogout from '@/hooks/useLogout';
 
 export const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 1024) {
-        setMenuOpen(false);
-      }
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
-    <header className="flex gap-4 justify-between px-[13vw] pt-5 cursor-pointer w-full">
-      <section className="hover:text-lime-500 text-white">
-        <Link href="/">
-          <div className="min-h-[30px] min-w-[30px]">
-            <Image
-              src="/homeLogo.svg"
-              alt="foto da logo portfolioManager"
-              width={40}
-              height={40}
-            />
-          </div>
+    <header className="w-full px-14 pt-4 lg:px-32 flex items-center justify-between">
+      <section>
+        <Link className="min-h-[30px] min-w-[30px]" href="/">
+          <Image
+            src="/homeLogo.svg"
+            alt="foto da logo portfolioManager"
+            width={40}
+            height={40}
+          />
         </Link>
       </section>
-      <nav
-        className={`lg:flex gap-4 [&>a]:text-white ${
-          menuOpen ? 'flex' : 'hidden'
-        } `}
-      >
-        <Link className="hover:text-lime-500" href="/">
-          Destaques
-        </Link>
-        <Link className="hover:text-lime-500" href="/">
-          Explorar
-        </Link>
+
+      <nav className=" [&>a]:text-white [&>a]:text-xs sm:[&>a]:text-base flex gap-4">
         <Link className="hover:text-lime-500" href="/">
           Procurar
         </Link>
@@ -61,20 +30,13 @@ export const Header = () => {
         >
           Trocar senha
         </Link>
-        <form action={useLogout}>
-          <button className="m-0 p-0 text-white hover:text-lime-500">
-            Logout
-          </button>
+        <form
+          className="text-xs sm:text-base text-white hover:text-lime-500 m-0 p-0"
+          action={useLogout}
+        >
+          <button>Logout</button>
         </form>
       </nav>
-      <div className="lg:hidden">
-        <button
-          className="text-white hover:text-lime-500"
-          onClick={handleMenuToggle}
-        >
-          {!menuOpen ? 'Menu' : 'Fechar'}
-        </button>
-      </div>
     </header>
   );
 };
